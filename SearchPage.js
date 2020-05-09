eeb8fbe55524eaa4155007a6c1e2d55a525b6f0e
Michelle Ellis <michelle.ellis@shopify.com>
2020-05-09 08:30:38 -0400

    search page UI

--- SearchPage.js
+++ SearchPage.js
@@ -23,6 +23,20 @@ export default class SearchPage extends Component<Props> {
         <Text style={styles.description}>
           Search by place-name or postcode.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            underlineColorAndroid={'transparent'}
+            style={styles.searchInput}
+            placeholder='Search via name or postcode'/>
+          <Button
+            onPress={() => {}}
+            color='#48BBEC'
+            title='Go'
+          />
+        </View>
+        <Image source={require('./Resources/house.png')} style={styles.image}/>
+
+
       </View>
     );
   }
@@ -40,4 +54,25 @@ const styles = StyleSheet.create({
     marginTop: 65,
     alignItems: 'center'
   },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC',
+  },
+  image: {
+    width: 217,
+    height: 138,
+  },
+  
 });
