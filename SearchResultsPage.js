9e9c33698b726c1d154e432b07952fef18f8d0f0
Michelle Ellis <michelle.ellis@shopify.com>
2020-05-11 21:45:52 -0400

    schnazy results

--- SearchResultsPage.js
+++ SearchResultsPage.js
@@ -10,20 +10,48 @@ import {
   Text,
 } from 'react-native';
 
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
+
+  render() {
+    const movie = this.props.item;
+    const year = movie.releaseYear;
+    return (
+      <TouchableHighlight
+        onPress={this._onPress}
+        underlayColor='#dddddd'>
+        <View>
+          <View style={styles.rowContainer}>
+            <View style={styles.textContainer}>
+              <Text style={styles.year}>{year}</Text>
+              <Text style={styles.title}
+                numberOfLines={1}>{movie.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+
 type Props = {};
 export default class SearchResultsPage extends Component<Props> {
   _keyExtractor = (item, index) => index.toString();
 
-  _renderItem = ({item}) => {
-    return (
-      <TouchableHighlight
-        underlayColor='#dddddd'>
-        <View>
-          <Text>{item.title}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-    
+  _renderItem = ({item, index}) => (
+    <ListItem
+      item={item}
+      index={index}
+      onPressItem={this._onPressItem}
+    />
+  );
+  
+  _onPressItem = (index) => {
+    console.log("Pressed row: "+index);
   };
 
   render() {
@@ -36,3 +64,27 @@ export default class SearchResultsPage extends Component<Props> {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  year: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+});
+
